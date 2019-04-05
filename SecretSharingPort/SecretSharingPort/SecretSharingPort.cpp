6bb70443963f3fafc61edfcf28d5bbd04338d9b7
Jonathan Takeshita <jtakeshi@nd.edu>
2019-04-05 16:23:30 -0400

    Debugging port

--- SecretSharingPort/SecretSharingPort/SecretSharingPort.cpp
+++ SecretSharingPort/SecretSharingPort/SecretSharingPort.cpp
@@ -14,8 +14,10 @@
 
 #ifdef __linux__
 #include <gmp.h>
+#include <gmpxx.h>
 #elif defined(WIN32)
 #include <mpir.h>
+#include <mpirxx.h>
 #else
 #error No OS defined!
 #endif
@@ -26,18 +28,20 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-void eval_at(mpz_t ret, const vector<mpz_t> & poly, mpz_t x);
-vector<pair<mpz_t, mpz_t> > make_random_shares(mpz_t ret, unsigned int minimum, unsigned int num_shares);
-void recover_secret(mpz_t ret, const std::vector<pair<mpz_t,mpz_t> > & shares);
-void divmod(mpz_t ret, mpz_t num, mpz_t den, mpz_t p);
-void lagrange_interpolate(mpz_t ret, mpz_t x, const std::vector<mpz_t> & x_s, const std::vector<mpz_t> & y_s);
-void PI(mpz_t ret, const std::vector<mpz_t> & vals);
+mpz_class eval_at(const vector<mpz_class> & poly, const mpz_class & x);
+vector<pair<mpz_class, mpz_class> > make_random_shares(mpz_class & ret, unsigned int minimum, unsigned int shares_length);
+mpz_class recover_secret(const std::vector<pair<mpz_class, mpz_class> > & shares);
+mpz_class divmod(const mpz_class & num, const mpz_class & den, const mpz_class & p);
+mpz_class lagrange_interpolate(const mpz_class & x, const std::vector<mpz_class> & x_s, const std::vector<mpz_class> & y_s);
+mpz_class PI(const std::vector<mpz_class> & vals);
+template<typename T>
+bool hasDuplicates(const vector<T> & vals);
 
 typedef std::chrono::high_resolution_clock myclock;
 myclock::time_point beginning = myclock::now();
 
 //static int prime = 13;
-static mpz_t prime;
+static mpz_class prime;
 
 /*
 void test_GCD(int argc, char ** argv){
@@ -56,8 +60,7 @@ void test_GCD(int argc, char ** argv){
 int main(int argc, char ** argv)
 {
   //Initialize the prime
-  mpz_init(prime);
-  mpz_set_ui(prime, 13);
+  prime = 13;
 
 	//test_GCD(argc, argv);
 
@@ -65,59 +68,55 @@ int main(int argc, char ** argv)
 	//char secret[11] = "secret_key";
 	//int secret = 1993;
 	//std::array<int, 6 > shares;
-	int shares_length = 6; //shares.size();
-	int minimum = 3;
+	unsigned int shares_length = 6; //shares.size();
+	unsigned int minimum = 3;
 	//int x_s, y_s;
 
   
-  mpz_t point;
-  mpz_init(point);	
+  mpz_class point;
 	auto shares_result = make_random_shares(point, minimum, shares_length);
 
-	std::cout << "secret: ";
-  gmp_printf("%Z\n");
+	std::cout << "secret: " << point << endl;
+  //gmp_printf("%Z\n");
 	std::cout << "shares:" << endl;
 	if (shares_length)
 	{
 		for (unsigned int i = 0; i < shares_result.size(); i++)
 		{
-      gmp_printf("%Z %Z\n", shares_result[i].first, shares_result[i].second);
+      //gmp_printf("%Z %Z\n", shares_result[i].first, shares_result[i].second);
+      cout << shares_result[i].first << ' ' << shares_result[i].second << endl;
 		}
 	}
 
 	std::cout << std::endl;
 
-	vector<pair<mpz_t,mpz_t> > firstThree(shares_result.begin(), shares_result.begin() + 3);
-	vector<pair<mpz_t,mpz_t> > secondThree(shares_result.begin()+3, shares_result.end());
+	vector<pair<mpz_class, mpz_class> > firstThree(shares_result.begin(), shares_result.begin() + 3);
+	vector<pair<mpz_class, mpz_class> > secondThree(shares_result.begin()+3, shares_result.end());
 
-  mpz_t result;
-  mpz_init(result);
+  mpz_class result;
 
-  recover_secret(result, firstThree);
-	std::cout << "secret recovered from a minimum subset of shares: ";
-  gmp_printf("%Z", result);
+  result = recover_secret(firstThree);
+	std::cout << "secret recovered from a minimum subset of shares: " << result << endl;
+  //gmp_printf("%Z", result);
 
-  recover_secret(result, secondThree);
-	std::cout << "secret recovered from another minimum subset of shares: ";
-  gmp_printf("%Z", result);
+  result = recover_secret(secondThree);
+	std::cout << "secret recovered from another minimum subset of shares: " << result << endl;
+  //gmp_printf("%Z", result);
 
   return 0;
-
 }
 
-void eval_at(mpz_t ret, const vector<mpz_t> & poly, mpz_t x){
-	mpz_t accum;
-	mpz_init(accum);
+mpz_class eval_at(const vector<mpz_class> & poly, const mpz_class & x){
+	mpz_class accum;
 	for(size_t i = poly.size(); i-- > 0; ){
-		mpz_mul(accum, accum, x);
-		mpz_add(accum, accum, poly[i]);
-		mpz_fdiv_r(accum, accum, prime);
+    accum *= x;
+    accum += poly[i];
+		mpz_fdiv_r(accum.get_mpz_t(), accum.get_mpz_t(), prime.get_mpz_t());
 	}
-  mpz_set(ret, accum);
-	return;
+  return accum;
 }
 
-vector<pair<mpz_t, mpz_t> > make_random_shares(mpz_t ret, unsigned int minimum, unsigned int shares_length){
+vector<pair<mpz_class, mpz_class> > make_random_shares(mpz_class & ret, unsigned int minimum, unsigned int shares_length){
 
 	if (minimum > shares_length)
 	{
@@ -125,8 +124,8 @@ vector<pair<mpz_t, mpz_t> > make_random_shares(mpz_t ret, unsigned int minimum, 
 		exit(1);
 	}
 
-	vector<mpz_t> poly;
-	vector<pair<mpz_t, mpz_t>> points;
+	vector<mpz_class> poly;
+	vector<pair<mpz_class, mpz_class>> points;
 
 	gmp_randstate_t state;
 	gmp_randinit_mt(state);
@@ -135,35 +134,26 @@ vector<pair<mpz_t, mpz_t> > make_random_shares(mpz_t ret, unsigned int minimum, 
 	unsigned seed = d.count();
 	gmp_randseed_ui(state, seed);
 
-	mpz_t generated;
-	mpz_init(generated);
-
+	
 	for(unsigned int i = 0; i < minimum; i++){
-		mpz_urandomb(generated, state, sizeof(unsigned int));
-		mpz_t tmp;
-		mpz_init(tmp);
-		mpz_set(tmp, generated);
-		mpz_fdiv_r(tmp, tmp, prime);
-		poly.push_back(tmp);
+    mpz_class generated;
+		mpz_urandomb(generated.get_mpz_t(), state, sizeof(unsigned int));
+		mpz_fdiv_r(generated.get_mpz_t(), generated.get_mpz_t(), prime.get_mpz_t());
+		poly.emplace_back(generated);
 	}
 
 	for(unsigned int i = 1; i < (shares_length+1); i++){
-		pair<mpz_t, mpz_t> point;
-    mpz_init(point.first);
-    mpz_init(point.second);
-		mpz_t mpz_i;
-		mpz_init(mpz_i);
-		mpz_set_ui(mpz_i, i);
-		eval_at(point.second, poly, mpz_i);
-		mpz_set(point.first, mpz_i);
-		points.push_back(point);
+    mpz_class point_first = i;
+    mpz_class point_second = eval_at(poly, point_first);
+		pair<mpz_class, mpz_class> point(point_first, point_second);
+		points.emplace_back(point);
 	}
 
-  mpz_set(ret, poly[0]);
+  ret = poly[0];
 	return points;
 }
 
-void recover_secret(mpz_t ret, const std::vector<pair<mpz_t,mpz_t> > & shares) {
+mpz_class recover_secret(const std::vector<pair<mpz_class, mpz_class> > & shares) {
 	//Recover the secret from share points (x, y points on the polynomial)
 
 	if (shares.size() < 2)
@@ -172,21 +162,17 @@ void recover_secret(mpz_t ret, const std::vector<pair<mpz_t,mpz_t> > & shares) {
 		exit(1);
 	}
 
-	std::vector<mpz_t> x_s, y_s;
-
+	std::vector<mpz_class> x_s, y_s;
 
 	x_s.reserve(shares.size());
 	y_s.reserve(shares.size());
 	for(auto & val : shares){
-		x_s.push_back(val.first);
-		y_s.push_back(val.second);
+		x_s.emplace_back(val.first);
+		y_s.emplace_back(val.second);
 	}
 
-	mpz_t zero;
-	mpz_init(zero);
-  lagrange_interpolate(ret, zero, x_s, y_s);
-  return;
-	//return lagrange_interpolate(zero, x_s, y_s); //Prime is currently global
+	mpz_class zero = 0;
+  return lagrange_interpolate(zero, x_s, y_s);
 }
 
 
@@ -201,55 +187,46 @@ void print_gcd(int a, int b, int x, int last_x, int y, int last_y, int quot){
 	cout << endl;
 }
 
-void divmod(mpz_t ret, mpz_t num, mpz_t den, mpz_t p) {
+mpz_class divmod(const mpz_class & num, const mpz_class & den, const mpz_class & p) {
 	//compute num / den modulo prime p
 	//To explain what this means, the return value will be such that
 	//the following is true : den * _divmod(num, den, p) % p == num
 
-	//std::pair<int, int> gcd_result = extended_gcd(den, p);
+  mpz_class g, s, t;
 
-  mpz_t g, s, t;
-  mpz_init(g);
-  mpz_init(s);
-  mpz_init(t);
+  //TODO Try using mpz_invert instead of the whole extended algorithm
+  mpz_gcdext(g.get_mpz_t(), s.get_mpz_t(), t.get_mpz_t(), den.get_mpz_t(), p.get_mpz_t());
 
-  //Try using mpz_invert instead of the whole extended algorithm
-  mpz_gcdext(g, s, t, den, p);
-
-  mpz_mul(s, s, num);
-  mpz_set(ret, s);
-  return;
+  return s*num;
 }
 
-void lagrange_interpolate(mpz_t ret, mpz_t x, const std::vector<mpz_t> & x_s, const std::vector<mpz_t> & y_s) {
+mpz_class lagrange_interpolate(const mpz_class & x, const std::vector<mpz_class> & x_s, const std::vector<mpz_class> & y_s) {
 	//Find the y - value for the given x, given n(x, y) points;
 	//k points will define a polynomial of up to kth order
 
 	size_t k = x_s.size();
-	std::set<int> set(begin(x_s), end(x_s));
-	if (k != (set.size()))
+
+	//std::set<int> set(begin(x_s), end(x_s));
+	if (hasDuplicates(x_s))
 	{
 		std::cout << "points must be distinct";
 		exit(1);
 	}
+
 	assert(x_s.size() == y_s.size() && "Vectors have nonequal size!");
 
-	std::vector<mpz_t> nums;  // avoid inexact division
-	std::vector<mpz_t> dens;
+	std::vector<mpz_class> nums;  // avoid inexact division
+	std::vector<mpz_class> dens;
 	nums.reserve(k);
 	dens.reserve(k);
 	//std::vector<int> others = x_s;
-	mpz_t cur;
-  mpz_init(cur);
+	mpz_class cur;
 
 	for (size_t i = 0; i < k; i++)
 	{
-		mpz_set(cur, x_s[i]);
-		//nums.pop_back();
-
-		mpz_t xo, curo;
-    mpz_init_set_si(xo, 1);
-    mpz_init_set_si(curo, 1);
+    cur = x_s[i];
+		mpz_class xo = 1;
+    mpz_class curo = 1;
 
 		for (size_t j = 0; j < k; j++)
 		{
@@ -257,64 +234,55 @@ void lagrange_interpolate(mpz_t ret, mpz_t x, const std::vector<mpz_t> & x_s, co
 				continue;
 			}
 
-      mpz_t xo_tmp;
-      mpz_init(xo_tmp);      
-      mpz_sub(xo_tmp, x, x_s[j]);
-      mpz_mul(xo, xo, xo_tmp);
+      mpz_class xo_tmp = x-x_s[j];
+      xo *= xo_tmp;
 
-
-      mpz_t curo_tmp;
-      mpz_init(curo_tmp);      
-      mpz_sub(curo_tmp, cur, x_s[j]);
-      mpz_mul(curo, curo, curo);
-
+      mpz_class curo_tmp = cur - x_s[j];
+      curo *= curo_tmp;
 		}
-
-		nums.push_back(xo);
-		dens.push_back(curo);
-
+		nums.emplace_back(xo);
+		dens.emplace_back(curo);
 	}
 
-	mpz_t	den;
-  mpz_init(den);
-  PI(den, dens);
-
-	mpz_t num;
-  mpz_init(num);
+	mpz_class	den = PI(dens);
+	mpz_class num = 0;
 
 	for (size_t idx = 0; idx < k; idx++) {
-
-    mpz_t result;
-    mpz_init(result);
-    mpz_mul(result, nums[idx], den);
-    mpz_mul(result, result, y_s[idx]);
-    mpz_fdiv_r(result, result, prime);
-    mpz_t divmod_result;
-    mpz_init(divmod_result);
-    divmod(divmod_result, result, dens[idx], prime);
-    mpz_add(num, num, divmod_result);
+    mpz_class intermediate = nums[idx] * den * y_s[idx];
+    mpz_fdiv_r(intermediate.get_mpz_t(), intermediate.get_mpz_t(), prime.get_mpz_t());
+    num += intermediate;
 	}
 
-  mpz_t tmp;
-  mpz_init(tmp);
-  divmod(tmp, num, den, prime);
-  mpz_add(tmp, tmp, prime);
-  mpz_fdiv_r(tmp, tmp, prime);
-  mpz_set(ret, tmp);
-	return;
-
+  mpz_class divmod_result = divmod(num, den, prime) + prime;
+  mpz_class ret;
+  mpz_fdiv_r(ret.get_mpz_t(), divmod_result.get_mpz_t(), prime.get_mpz_t());
+	return ret;
 }
 
-void PI(mpz_t ret, const std::vector<mpz_t> & vals){
+mpz_class PI(const std::vector<mpz_class> & vals){
 	// upper - case PI -- product of inputs
-	mpz_t accum;
-  mpz_init(accum);
-  mpz_set_si(accum, 1);
-
-	for (const auto & x: vals)
+	mpz_class accum = 1;
+	for(const auto & x: vals)
 	{
-		mpz_mul(accum, accum, x);
+		accum += x;
 	}
-  mpz_set(ret, accum);
-	return;
+	return accum;
 }
+
+template<typename T>
+bool hasDuplicates(const vector<T> & vals){
+  for(size_t i = 0; i < vals.size(); i++){
+    for(size_t j = 0; j < vals.size(); j++){
+      if(i == j){
+        continue;      
+      }    
+      if(vals[i] == vals[j]){
+        return true;      
+      }
+    }  
+  }
+  return false;
+}
+
+
+
