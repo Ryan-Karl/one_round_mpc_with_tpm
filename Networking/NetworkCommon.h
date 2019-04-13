08664befc1f5d38c933736b5951f99679b9a684b
Jonathan Takeshita <jtakeshi@nd.edu>
2019-04-12 23:48:55 -0400

    Added function to extract RSA key from file

--- Networking/NetworkCommon.h
+++ Networking/NetworkCommon.h
@@ -2,16 +2,23 @@
 #define NETWORKCOMMON_H
 
 #include <string>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 #define DEFAULT_PORTNUM 27015
 #define DEFAULT_PORT "27015"
 #define DEFAULT_BUFFER_LENGTH	1024
 #define LABELS_EXTENSION ".tsv"
 #define FILE_DELIM '~'
-#define 
+#define KEYBUFFER 128
+
+#ifdef WIN32
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+
 
 typedef struct{
 	unsigned int partyNum;
@@ -95,7 +102,48 @@ int SendFile(int * ret, const std::string & filename, SOCKET & ClientSocket){
 	closesocket(ClientSocket);
 	return *ret = 0;
 }
+#else
+typedef unsigned char BYTE;
+#endif //End of Windows-only code
 
+//Get RSA key from keyfile
+//TODO set parameters based on key size - hardcoded right now
+//TODO finish
+std::vector<BYTE> keyFromFile(const std::string & filename){
+	std::string str;
+	std::vector<BYTE> vec;
+	std::ifstream ifs(filename);
+	if(!ifs.good()){
+		std::cerr << "ERROR: could not read from file " << filename << std::endl;
+	}
+	vec.reserve(KEYBUFFER);
+	while(ifs >> str){
+		if(str == "buffer"){
+			break;
+		}
+	}
+	ifs >> str; //Read in the =
+	for(unsigned int i = 0; i < 32; i++){
+		ifs >> str;
+		if(!i){
+			str = str.substr(1, str.size()-1);
+		}
+		else{
+			if(i == 31){
+				str = str.substr(0, str.size()-1);
+			}
+		}
+		std::istringstream iss(str);
+		unsigned int holder = 0;
+		iss >> std::hex >> holder;
+		vec.push_back((BYTE)(holder >> 24) & 0xFF);
+		vec.push_back((BYTE)(holder >> 16) & 0xFF);
+		vec.push_back((BYTE)(holder >> 8) & 0xFF);
+		vec.push_back((BYTE) holder & 0xFF);
+	}
+
+	return vec;
+}
 
 
 
