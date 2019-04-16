985931958286ca88fea7cdcfe9e84689e95360fb
T. Burchfield <tburchfi@nd.edu>
2019-04-16 08:27:28 -0400

    Add structure to garble code

--- Garbler/garble_util.cpp
+++ Garbler/garble_util.cpp
@@ -2,28 +2,38 @@
 #include <math.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "garble_util.h"
 
 using namespace std;
 
-//Called by server
+// Called by server.  Expects the structure of the circuit but not values relevant to garbling.
 void get_garbled_circuit(Circuit * c, PlayerInfo ** players) {
   //Get randomness from player info
+  wire_value * R = random_wire(c->security);
+  for (Wire * i = c->input_wires[0]; i != NULL; i++) {
+    bit p0 = random_bit();
+    bit p1 = xor_bit(p0, constbit_1);
+    wire_value * k0 = random_wire(c->security);
+    wire_value * k1 = xor_wire(k0, R);
 
-/*
-[INCOMPLETE] pseudocode
-assign_masks(wire):
-  R = newRAND(N)
-  For all input wires i (i.e. leaves):
-    //Permutation bit, key
-    p0 = newRANDbit
-    k0 = newRAND(N)
-    p1 = p0 ^ 1
-    k1 = k0 ^ R
-    w0 = k0 ++ p0
-    w1 = k1 ++ p1
-  For each gate in topological with inputs a, b:
-*/
-
+    i->p0 = p0;
+    i->p1 = p1;
+    i->k0 = k0;
+    i->k1 = k1;
+  }
+  queue<Wire *> t_ordering;
+  //TODO: get topological ordering
+  while (!t_ordering.empty()) {
+    Wire * w = t_ordering.pop();
+    if (w->is_gate && w->gate_type == GATE_XOR) {
+      //TODO
+    } else if (w->is_gate) {
+      //TODO
+    }
+  }
+  for (Wire * i = c->output_wires[0]; i != NULL; i++) {
+    //TODO: create garbled output table
+  }
 }
 
 
