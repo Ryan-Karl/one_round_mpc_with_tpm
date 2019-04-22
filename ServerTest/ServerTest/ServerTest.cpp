c3405012a726a902ce751374bf319fe8f0e65487
Ryan Karl <rkarl@nd.edu>
2019-04-22 18:50:05 -0400

    Update

--- ServerTest/ServerTest/ServerTest.cpp
+++ ServerTest/ServerTest/ServerTest.cpp
@@ -1,48 +1,101 @@
 // ServerTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
-#include <iostream>
 
-#include "Networking.h"
-#include "utilities.h"
+
+#include "pch.h"
+
+#define NOMINMAX
+#include <iostream>
+#include "../includes/NetworkUtils.h"
+#include "../includes/utilities.h"
+#include "../includes/TPMWrapper.h"
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <string.h>
 
 #define BASEFILE "basefile.txt"
 #define ENCFILE "encfile.txt"
 #define KEYFILE "keyfile.txt"
+#define NUMPARTIES_DEFAULT 1
+#define SERVER_TPM_PORT 31000
 
 using namespace std;
 
-
+//First arg is port, second is a string to encrypt, third is the TPM port
 int main(int argc, char ** argv) {
-	if (argc < 1) {
-		cout << "ERROR: not enough files" << endl;
+	
+	if (argc < 4) {
+		cout << "First arg is port, second is a string to encrypt, third is the TPM port" << endl;
 		return 0;
 	}
 
+	Server s(atoi(argv[1]));
+	s.init();
+	s.accept_connections(1);
+	char * keystr;
+	unsigned int strLen;
+	s.recvString(0, strLen, (char **)&keystr);
+	TPMWrapper myTpm(atoi(argv[3]));
+	string jsonStr(keystr);
+	auto key = myTpm.s_readKey(jsonStr);
+	vector<BYTE> toEncrypt = stringToByteVec(argv[2], strlen(argv[2]));
+	vector<BYTE> encryptedVec = myTpm.s_RSA_encrypt(toEncrypt, key);
+	string encStr = ByteVecToString(encryptedVec);
+	s.sendString(0, encStr.size()+1, encStr.c_str());
+	s.stop();
+
+
+
+	
+	/*
 	Client c(LOCALHOST, DEFAULT_PORTNUM);
-	c.Start();
+	if (!c.Start()) {
+		cout << "Error starting client" << endl;
+		return 1;
+	}
 	//Accept plaintext, then the key
 	vector<string> filenames;
 	filenames.push_back(BASEFILE);
 	filenames.push_back(KEYFILE);
-	RecvDelimitedFiles(filenames, c.getSocket);
+	SOCKET mySock = c.getSocket();
+	RecvDelimitedFiles(filenames, mySock);
+  cout << "Received files" << endl;
+  c.Stop();
+  //Start server immediately
+  Server s(DEFAULT_PORTNUM);
+  if (s.init()) {
+	  cout << "ERROR: server init" << endl;
+  }
+  else { cout << "Started server" << endl; }
+  if (s.accept_connections(NUMPARTIES_DEFAULT)) {
+	  cout << "ERROR: accept" << endl;
+  }
+  else { cout << "Accepted connection" << endl; }
+
 	//Read in and decrypt file
-	TPMWrapper myTPM;
-	myTPM.s_readKeyFromFile(KEYFILE);
+	TPMWrapper myTPM(argc >= 2? atoi(argv[1]) : DEFAULT_TPM_PORT);
+	auto key = myTPM.s_readKeyFromFile(KEYFILE);
 	ifstream bfs(BASEFILE);
 	auto tmp = vectorsFromHexFile(bfs);
 	auto plaintext = flatten(tmp);
-	auto ciphertext = s_RSA_encrypt(plaintext);
+
+	//Read key from file
+	auto ciphertext = myTPM.s_RSA_encrypt(plaintext, key);
+
 	//Write out and send file
 	ofstream os(ENCFILE);
 	outputToStream(os, ciphertext);
+
 	int trash;
-	SendFile(&trash, ENCFILE, c.getSocket());
+  
+	SendFile(&trash, ENCFILE, mySock);
+  s.close_connections();
+  */
 
 
 	return 0;
 }
+
