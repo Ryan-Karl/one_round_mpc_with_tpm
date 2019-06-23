5907088ffa13d2c19b43686ddfa5f7d1ce1cd498
Ryan-Karl <48491306+Ryan-Karl@users.noreply.github.com>
2019-06-23 19:17:11 +0000

    Revert "Handle special case with only one share"
    This reverts commit b8c1a935f62c9d83bb8794860d511965ed36f734.

--- includes/XOR_sharing.h
+++ includes/XOR_sharing.h
@@ -3,11 +3,10 @@
 
 #include <vector>
 #include <cstdlib>
-#include <exception>
+#include <stdexcept>
 
 using std::vector;
 
-//NB extremely insecure, for prototyping only
 void fill_vector_rand(std::vector<unsigned char> & v){
 	for(size_t i = 0; i < v.size(); i++){
 		v[i] = (unsigned char) rand();
@@ -24,16 +23,11 @@ void vector_xor(std::vector<unsigned char> & left, const std::vector<unsigned ch
 }
 
 std::vector<std::vector<unsigned char> > get_shares(unsigned int num_shares, const std::vector<unsigned char> & secret){
-	if(!num_shares){
+	if(num_shares < 2){
 		throw std::logic_error("Not enough shares!");
 	}
 	std::vector<std::vector<unsigned char> > ret;
 	ret.resize(num_shares);
-	//Handle the case of only one share
-	if(num_shares == 1){
-		ret[0] = secret;
-		return ret;
-	}
 	for(size_t i = 0; i < num_shares - 1; i++){
 		ret[i].resize(secret.size());
 		fill_vector_rand(ret[i]);
@@ -49,7 +43,7 @@ std::vector<std::vector<unsigned char> > get_shares(unsigned int num_shares, con
 }
 
 std::vector<unsigned char> recover_secret(const std::vector<std::vector<unsigned char> > & shares){
-	if(!shares.size()){
+	if(shares.size() < 2){
 		throw std::logic_error("Not enough shares to reconstruct!");
 	}
 	std::vector<unsigned char> ret = shares[0];
