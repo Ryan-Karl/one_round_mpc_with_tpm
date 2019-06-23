528f0f354850d30d22220898192fff024f7c0d20
Ryan-Karl <48491306+Ryan-Karl@users.noreply.github.com>
2019-06-23 19:12:43 +0000

    Fixed Client Bugs

--- TSS.MSR/TSS.CPP.Client/Samples/ClientTest.cpp
+++ TSS.MSR/TSS.CPP.Client/Samples/ClientTest.cpp
@@ -467,7 +467,7 @@ void client_connect(unsigned int me, const std::string& hostname, unsigned int p
 	//First, receive which party the message is from
 	unsigned int* them;
 	unsigned int partyLen;
-	if (c.recvBuffer((void**)& them, partyLen) || partyLen != sizeof(them)) {
+	if (c.recvBuffer((void**)& them, partyLen) || partyLen != sizeof(*them)) {
 		cerr << "ERROR : receiving" << hostname << ' ' << port << endl;
 		throw new std::exception("ERROR receiving");
 	}
@@ -545,10 +545,11 @@ void server_connect(Server& s, unsigned int num_cons, unsigned int me,
 		//First, receive which party the message is from
 		unsigned int* them;
 		unsigned int partyLen;
-		if (s.recvBuffer(i, (void**)& them, partyLen) || partyLen != sizeof(them)) {
+		if (s.recvBuffer(i, (void**)& them, partyLen) || partyLen != sizeof(unsigned int)) {
 			cerr << "ERROR : receiving" << endl;
 			throw new std::exception("ERROR receiving");
 		}
+		std::cout << "Server sent party number" << std::endl;
 		//Next, get the actual data
 		//char * recvData;
 		unsigned int dataLen;
