61dfafc0e5f65e07db25fcd3b8fa40f67d29fce1
Jonathan Takeshita <jtakeshi@nd.edu>
2019-11-16 20:57:10 -0500

    Forgot a continue statement

--- online-phase-simulator/simulator.cpp
+++ online-phase-simulator/simulator.cpp
@@ -32,6 +32,7 @@ int main(int argc, char ** argv){
 		}
 		if(!strcmp(argv[argx], "--append")){
 			append = true;
+			continue;
 		}
 		if(!strcmp(argv[argx], "-p")){
 			port = atoi(argv[++argx]);
